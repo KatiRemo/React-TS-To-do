df1b70d6195f40398a754763d3ffc81c7bea22d9
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2022-03-04 14:09:22 +0200

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoList from './components/ToDoList';
+import NewTodo from './components/NewToDo';
+import {Todo} from './toDo.model'
 
-function App() {
+const App: React.FC = () => {
+  const [todoState, setTodoState] = useState<string>('add');
+  const [todo, setTodo] = useState<Todo>({ id: '', text: '' });
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const onSubmitTodo = (text: string) => {
+    if (todoState === 'edit') {
+      setTodos((prevTodos) => {
+        return prevTodos.map((prevTodo) => {
+          if (prevTodo.id === todo.id) {
+            return todo;
+          } else {
+            return prevTodo;
+          }
+        });
+      });
+    } else {
+      setTodos((prevTodos) => [
+        ...prevTodos,
+        { id: Math.random().toString(), text: text },
+      ]);
+    }
+    setTodo((prevTodo) => {
+      return { ...prevTodo, id: '', text: '' };
+    });
+    setTodoState(() => {
+      return '';
+    });
+  };
+
+  const todoChangeHandler = (text: string) => {
+    setTodo((prevTodo) => {
+      return { ...prevTodo, text: text };
+    });
+  };
+
+  const todoEditHandler = (id: string) => {
+    setTodo((prevTodo) => {
+      const todo = todos.filter((todo) => todo.id === id);
+      const text = todo.length ? todo[0].text : '';
+      return { ...prevTodo, id: id, text: text };
+    });
+    setTodoState(() => {
+      return 'edit';
+    });
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((prevTodo) => prevTodo.id !== todoId);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodo
+        text={todo.text}
+        state={todoState}
+        onChangeTodo={todoChangeHandler}
+        onSubmitTodo={onSubmitTodo}
+      />
+      <TodoList
+        items={todos}
+        onEditTodo={todoEditHandler}
+        onDeleteTodo={todoDeleteHandler}
+      />
     </div>
   );
-}
+};
 
 export default App;
